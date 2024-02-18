8d97668a1617d9b93d8c955619b5980fe6b76396
Apeshits <1529168905@qq.com>
2024-02-18 10:45:54 +0800

    first

---
+++ app/build/generated/source/buildConfig/debug/com/xindawn/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.xindawn;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "net.basicgo.tvcast";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 28;
+  public static final String VERSION_NAME = "2.0";
+}
